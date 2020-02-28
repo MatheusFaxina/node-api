a9b93248218e4badf5d5f609fece39866354d5bd
Matheus Faxina <matheus.vfaxina@gmail.com>
2020-02-27 22:21:54 -0300

    Adicionando o projeto ao Github

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'ae01f774c2ae5d288522a1615d20cf1f',
+  expiresIn: '7d',
+}
---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '192.168.99.100',
+  port: '32771',
+  username: 'postgres',
+  password: '99729088',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  }
+};
